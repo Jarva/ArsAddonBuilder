dd7fa6eb97a805baecdd4c1dac5872074d4b5012
Jarva <4622609+Jarva@users.noreply.github.com>
2024-06-17 02:07:39 +0100

    fix: refactor to use just repo slug

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,10 +2,10 @@
 
 find "projects" -type f | while read -r file; do
     module=$(jq -c "." $file)
-    url=$(echo "$module" | jq -cr ".url")
+    repo=$(echo "$module" | jq -cr ".repo")
     branch=$(echo "$module" | jq -cr ".branch")
     current=$(echo "$module" | jq -cr ".hash")
-    head=$(git ls-remote "$url" "refs/heads/$branch" | cut -f 1)
+    head=$(git ls-remote "https://github.com/$repo.git" "refs/heads/$branch" | cut -f 1)
     if [ "$current" != "$head" ]; then
         echo "Updating $file..."
         echo "$module" | jq '.hash = $hash' --arg hash "$head" > $file
