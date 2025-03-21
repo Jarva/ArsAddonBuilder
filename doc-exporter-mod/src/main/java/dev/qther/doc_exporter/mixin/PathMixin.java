cb2c4711c5de98537ec768fabd59afc9ae23be14
Qther <qther@tuta.io>
2025-03-22 00:47:19 +0800

    fix: attempt 17

--- doc-exporter-mod/src/main/java/dev/qther/doc_exporter/mixin/PathMixin.java
+++ doc-exporter-mod/src/main/java/dev/qther/doc_exporter/mixin/PathMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import java.nio.file.Path;
 
 @Mixin(value = Path.class, remap = false)
-public class PathMixin {
+public interface PathMixin {
     @WrapMethod(method = "of(Ljava/lang/String;[Ljava/lang/String;)Ljava/nio/file/Path;")
     private static Path wikiPath(String first, String[] more, Operation<Path> original) {
         first = first.replaceFirst("^\\.\\./\\.\\./wiki", "./wiki");
