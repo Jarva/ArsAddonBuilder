019574336c698ee39cf0bac3a2ae8880c949fcae
Qther <qther@tuta.io>
2025-03-21 23:41:16 +0800

    fix: attempt 12

--- doc-exporter-mod/src/main/java/dev/qther/doc_exporter/DocExporter.java
+++ doc-exporter-mod/src/main/java/dev/qther/doc_exporter/DocExporter.java
@@ -1,8 +1,10 @@
 package dev.qther.doc_exporter;
 
+import net.minecraft.SharedConstants;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +13,6 @@ public class DocExporter {
     public static final String MODID = "doc_exporter";
     public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
-    public DocExporter(IEventBus modEventBus, ModContainer modContainer) {}
+    public DocExporter(IEventBus modEventBus, ModContainer modContainer) {
+    }
 }
